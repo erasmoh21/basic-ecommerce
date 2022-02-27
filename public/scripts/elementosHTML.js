98cd9bfc82c0b9e93f9444e66faa8ba041c0eced
= <=>
2022-02-27 17:59:38 -0500

    se terminaron algunas estilizaciones y dinamizaciones con javascript en cuanto el carro de compras y ver las diferentes imagenes de las fotos que estan en miniaturas pasarlas a la caja mas grande para la vista del usuario

--- public/scripts/elementosHTML.js
+++ public/scripts/elementosHTML.js
@@ -1,11 +1,15 @@
 const elementosHtml = {
     iconoCarro: document.getElementsByClassName('contenedorCartIcon'),
-    bannerListaProductos : document.querySelector('.contenedorElementosBanner img'),
-    contenedorZapatoIndividual: document.querySelector('.contenedorZapatoIndividual img'),
+    bannerListaProductos : document.getElementsByClassName('contenedorElementosBanner'),
+    contenedorZapatoIndividual: document.querySelector('.imgIndividual'),
     contenedorDeZapatos: document.getElementsByClassName('contenedorDeZapatos'),
     botonDisminuir: document.querySelector('.disminucion'),
     botonAumento: document.querySelector('.aumento'),
     cantidadDeProducto: document.querySelector('.numero'),
-    botonAniadirAlCarro: document.querySelector('.btn-aniadirCarro')
+    botonAniadirAlCarro: document.querySelector('.btn-aniadirCarro'),
+    formulario: document.getElementById('form'),
+    numeroDeProductos: document.querySelector('.numeroDeProductos'),
+    contenedorListaCarrito: document.querySelector('.contenedorListaCarrito'),
+    contenedorHeader2: document.querySelector('.contenedorHeader-2')
 }
 export default elementosHtml
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,7 @@
 import elementosHtml from './elementosHTML.js'
+import plantillaDeProductosEnCarrito from './plantillaDeProductosEnCarrito.js'
+
+let cantidadDeProducto = parseInt(elementosHtml.cantidadDeProducto.textContent) 
 
 elementosHtml.iconoCarro.item(0).addEventListener('click', () => {
     if(elementosHtml.bannerListaProductos.item(0).style.display == "flex") {
@@ -13,16 +16,19 @@ elementosHtml.contenedorDeZapatos.item(0).addEventListener('click',(e) => {
     if(e.target && e.target.tagName == 'IMG') {
         const imagenMiniatura = e.target.getAttribute('src')
         elementosHtml.contenedorZapatoIndividual.setAttribute('src',imagenMiniatura)
+        console.log(imagenMiniatura)
     }
 })
 
-elementosHtml.botonAumento.addEventListener('click',() => {
+elementosHtml.botonAumento.addEventListener('click',(e) => {
+    e.preventDefault()
     let numeroAumento = parseInt(elementosHtml.cantidadDeProducto.textContent)
     numeroAumento++
     elementosHtml.cantidadDeProducto.textContent = numeroAumento 
 })
 
-elementosHtml.botonDisminuir.addEventListener('click',() => {
+elementosHtml.botonDisminuir.addEventListener('click',(e) => {
+    e.preventDefault()
     if(parseInt(elementosHtml.cantidadDeProducto.textContent) == 0) {
         return
     }
@@ -31,4 +37,21 @@ elementosHtml.botonDisminuir.addEventListener('click',() => {
         numeroDisminucion--
         elementosHtml.cantidadDeProducto.textContent = numeroDisminucion
     }
+})
+
+elementosHtml.formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if(elementosHtml.cantidadDeProducto.textContent == '0') {
+        return
+    }
+    else {
+        elementosHtml.numeroDeProductos.innerHTML = `<p>${cantidadDeProducto+=parseInt(elementosHtml.cantidadDeProducto.textContent)}</p>`
+        elementosHtml.contenedorListaCarrito.outerHTML = plantillaDeProductosEnCarrito(parseInt(cantidadDeProducto)) 
+        elementosHtml.numeroDeProductos.style.display = "block"
+        elementosHtml.cantidadDeProducto.textContent = '0'
+    }
+})
+
+elementosHtml.contenedorZapatoIndividual.addEventListener('click',() => {
+    console.log('hola')
 })
